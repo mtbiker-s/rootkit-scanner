418097cfb560bc9575b76a410507dacee5a28cd6
ceSanchez <carloesanchez@gmail.com>
2020-07-19 10:19:03 -0700

    README and logic changes

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
-# rootkit-scan 
-This is just something to help scan for rootkits taking advantage of rkhunter program. It really stemed from trying to lock down devices at home with somany security exploits being generated during the COVID-19 timeframe.
+# rootkit-scanner
+This is just something to help scan for rootkits taking advantage of rkhunter program. It really stemed from trying to lock down devices at home with so many security exploits being generated during the COVID-19 timeframe.
 
 Inspirations:
 
@@ -10,9 +10,9 @@ Setup a Rust script that will call rkhunter (rootkit hunter) and then run in Jen
 
 ## Requirements
 
-* Rust - 
-* Jenkins - 
-* rkhunter -
+* Rust - [https://www.rust-lang.org/](https://www.rust-lang.org/)
+* Jenkins - [https://www.jenkins.io/](https://www.jenkins.io/)
+* rkhunter - [http://rkhunter.sourceforge.net/](http://rkhunter.sourceforge.net/)
 
 All this will run on RaspberryPi setups, should run on other Linux OSes as well. 
 
--- src/main.rs
+++ src/main.rs
@@ -4,30 +4,37 @@ fn main() {
     println!("Attempting to scan for rootkits with rkhunter");
 
     update();
-    scan();
-
-    println!("rootkit scan completed!")
+	scan();
 }
 
-fn update(){
-   
-   Command::new("sudo")
-	.arg("rkhunter")
-   	.arg("--update")
-        .spawn()
-        .expect("rkhunter update failed to start");
+fn update() {
+  	let mut rkh  = Command::new("rkhunter");
+
+	println!("");
+	println!("Attempting to update rkhunter...");
+	rkh.arg("--update");
+	rkh.status().expect("process failed to execute");
+
+	println!();
+	println!("rkhunter update completed!");
  	
 }
 
-fn scan(){
-   Command::new("sudo")
-	.arg("rkhunter")
-	.arg("-c")
-	.arg("--enable")
-	.arg("all")
-	.arg("--rwo")
-	.arg("--sk")
-	.spawn()
-	.expect("rkhunter scan failed.");
+fn scan() {
+	let mut rkh  = Command::new("rkhunter");
+
+	println!("");
+	println!("Attempting to rootkit scan with rkhunter...");
+	// Execute `ls` in the current directory of the program.
+	
+	rkh.arg("-c");
+	rkh.arg("--enable");
+	rkh.arg("all");
+	rkh.arg("--rwo");
+	rkh.arg("--sk");
+	rkh.status().expect("rkhunter failed to execute!");
+
+	println!();
+	println!("rootkit scan completed!");
 
 }
